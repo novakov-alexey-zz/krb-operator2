f7af087b2f7a9ddf0fe12f12789df795af9ddc0c
Alexey Novakov <novakov.alex@gmail.com>
2021-09-23 08:45:14 +0200

    remove unused tests

--- src/test/scala/freya/HelloSpec.scala
+++
@@ -1,8 +0,0 @@
-package freya
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class HelloSpec extends AnyFlatSpec with Matchers {
-  "The Hello object" should "say hello" in {}
-}
