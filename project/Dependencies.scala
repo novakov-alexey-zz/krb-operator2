41134579724f193bd403984efe50005c0faaa9f0
Scala Steward <me@scala-steward.org>
2022-01-31 18:34:57 +0100

    Update logback-classic to 1.3.0-alpha13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
 
   lazy val logbackClassic =
-    "ch.qos.logback" % "logback-classic" % "1.3.0-alpha12"
+    "ch.qos.logback" % "logback-classic" % "1.3.0-alpha13"
 
   val pureConfigVersion = "0.17.1"
   lazy val pureConfig =
